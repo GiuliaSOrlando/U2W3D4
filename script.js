3e6e066a3b473efeee6d87742e9073e24a177450
GiuliaSOrlando <134381769+GiuliaSOrlando@users.noreply.github.com>
2023-07-06 17:37:44 +0200

    edit

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const myURL = `https://api.pexels.com/v1/search?query=kittens`
 const myURL2 = `https://api.pexels.com/v1/search?query=finland`
 
-const loadImages = function () {
-  fetch(myURL, {
+let searchForm = document.getElementById("search-form")
+searchForm.addEventListener("submit", function (e) {
+  e.preventDefault()
+  const query = document.getElementById("search")
+  let queryValue = query.value
+  const dynamicURL = `https://api.pexels.com/v1/search?query=${queryValue}`
+  console.log(dynamicURL)
+  let myURL3 = dynamicURL
+  loadImages(myURL3)
+})
+
+const loadImages = function (url) {
+  fetch(url, {
     headers: {
       Authorization: "dPp5EaxCkWtKMddcH79nua2VcYg7nVbe37RSaBf7W7PBRTTnZI2jpuVI",
     },
@@ -66,66 +77,12 @@ const loadImages = function () {
     })
 }
 
-const loadSecondaryImages = function () {
-  fetch(myURL2, {
-    headers: {
-      Authorization: "dPp5EaxCkWtKMddcH79nua2VcYg7nVbe37RSaBf7W7PBRTTnZI2jpuVI",
-    },
-  })
-    .then((res) => {
-      console.log(res)
-      if (res.ok) {
-        return res.json()
-      } else {
-        throw new Error("Errore")
-      }
-    })
-    .then((finland) => {
-      const finlandArr = finland.photos
-      finlandArr.forEach((photo) => {
-        const imgRow = document.getElementById("img-row")
-        let newCol = document.createElement("div")
-        newCol.classList.add("col-md-4")
-        newCol.innerHTML = `
-        <div class="card mb-4 shadow-sm">
-                <img src="${photo.src.medium}" alt="">
-                <div class="card-body">
-                  <h5 class="card-title">${photo.photographer}</h5>
-                  <p class="card-text">
-                    ${photo.salt}
-                  </p>
-                  <div
-                    class="d-flex justify-content-between align-items-center"
-                  >
-                    <div class="btn-group">
-                      <button
-                        type="button"
-                        class="btn btn-sm btn-outline-secondary"
-                      >
-                        View
-                      </button>
-                      <button
-                        type="button"
-                        class="btn btn-sm btn-outline-secondary"
-                      >
-                        Edit
-                      </button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                  </div>
-                </div>
-              </div>
-        `
-        imgRow.appendChild(newCol)
-      })
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-}
-
 let firstLoadBtn = document.getElementById("first-load-image")
-firstLoadBtn.addEventListener("click", loadImages)
+firstLoadBtn.addEventListener("click", function () {
+  loadImages("https://api.pexels.com/v1/search?query=kittens")
+})
 
 let secondLoadBtn = document.getElementById("second-load-image")
-secondLoadBtn.addEventListener("click", loadSecondaryImages)
+secondLoadBtn.addEventListener("click", function () {
+  loadImages("https://api.pexels.com/v1/search?query=finland")
+})
